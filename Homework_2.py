acc39236a008d69720c45b44e315cb0160d7178a
arpi-gh <123762164+arpi-gh@users.noreply.github.com>
2023-01-30 20:08:10 +0400

    Update Homework_2.py

--- Homework_2.py
+++ Homework_2.py
@@ -1,1 +1,8 @@
-
+# Basic operations  in interactive prompt using built-in types in Python (Number, String, List, Tuple, Dictionary, Set).
+# Write a program that generates and prints 50 random integers, each between 3 and 6.
+# Write a program that asks the user to enter two numbers, x and y , and computes | x − y | /  x + y
+# Write a program that checks if a string is a palindrome.
+# Write a program that takes a list of numbers and calculates the sum of its elements.
+# Write a program that takes a list of numbers and returns the largest number in the list.
+# Write a program that reverses a string.
+# Write a program that calculates the area of a circle given its radius.
